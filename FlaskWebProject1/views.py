461e8d45223862cd057365240eef48d99ffeca3a
ip98@list.ru <paul_d13Ivan>
2017-12-17 03:20:20 +0500

    Possibility to comment a web page.

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -1,9 +1,19 @@
 """
 Routes and views for the flask application.
 """
-
-from flask import render_template
+from collections import namedtuple
+from datetime import datetime
+from flask import render_template, request
 from FlaskWebProject1 import app
+from threading import Timer
+from werkzeug.utils import redirect
+
+FeedbackInfo = namedtuple("FeedbackInfo", "sender date text")
+
+FEEDBACK_STORAGE = {"comments": [],
+                    "news": [],
+                    "contact": []}
+LAST_VISITED_IP = set()
 
 
 @app.route("/")
@@ -25,3 +35,19 @@ def contact():
 @app.route("/news")
 def news():
     return render_template("news.html")
+
+
+@app.route("/comments", methods=["GET", "POST"])
+def comments():
+    if request.method == 'POST':
+        ip_from = request.remote_addr
+        if request.remote_addr in LAST_VISITED_IP:
+            return redirect(request.full_path)
+        LAST_VISITED_IP.add(ip_from)
+        Timer(10, lambda: LAST_VISITED_IP.remove(ip_from)).start()
+        record = FeedbackInfo(request.form['nickname'], datetime.now(),
+                              request.form['text_area'])
+        FEEDBACK_STORAGE[request.args["from"]].append(record)
+        return redirect(request.full_path)
+    return render_template("comments.html",
+                           data=FEEDBACK_STORAGE[request.args["from"]])
