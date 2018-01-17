3cbc586a220a2b610ebcbcc2fd5f81974fc96b1d
ip98@list.ru <paul_d13Ivan>
2018-01-17 21:39:24 +0500

    Add tags filtering + usage.\nCollect info about user.

--- FlaskWebProject1/views.py
+++ FlaskWebProject1/views.py
@@ -6,7 +6,7 @@ from datetime import datetime
 from flask import render_template, request, json
 from FlaskWebProject1 import app
 from FlaskWebProject1.ip_workers import IpTracker, IpStorage
-from re import sub
+from FlaskWebProject1.text_filter import StringProcessor
 
 FeedbackInfo = namedtuple("FeedbackInfo", "sender date text")
 
@@ -16,6 +16,7 @@ FEEDBACK_STORAGE = {"comments": [],
 
 ip_storage = IpStorage()
 ip_tracker = IpTracker()
+string_processor = StringProcessor()
 
 
 @app.route("/")
@@ -44,24 +45,32 @@ def comments():
     ip_from = request.remote_addr
     if ip_from not in ip_tracker.last_visited_ip:
         ip_tracker.track_ip(ip_from, posted=False)
-        ip_storage.update(ip_from not in ip_tracker.unique_ips)
+        if request.method != 'POST':
+            ip_storage.update(ip_from not in ip_tracker.unique_ips)
 
     ip_tracker.update_unique(ip_from)
 
     if request.method == 'POST':
         if ip_from not in ip_tracker.last_posted_ip and request.form['text_area']:
-            ip_tracker.track_ip(ip_from, posted=True)
             nickname = request.form['nickname']
             if not nickname:
                 nickname = 'Anonymous'
-            else:
-                nickname = sub('<[^<]+?>', '', request.form['nickname'])
-            feedback = sub('<[^<]+?>', '', request.form['text_area'])
+            ip_tracker.track_ip(ip_from, posted=True)
+            nickname = string_processor.shield(nickname)
+            feedback = string_processor.shield(request.form['text_area'])
             record = FeedbackInfo(nickname, datetime.now(), feedback)
             FEEDBACK_STORAGE[request.args["from"]].append(record)
         return json.dumps(FEEDBACK_STORAGE[request.args["from"]])
 
+    resolution = "%sx%s" % (request.cookies['width'],
+                            request.cookies['height'])
+
+    browser = "%s %s" % (request.user_agent.browser,
+                         request.user_agent.version.split('.')[0])
+
     return render_template("comments.html",
                            data=FEEDBACK_STORAGE[request.args["from"]],
                            visit_info=ip_storage.get_stats(),
-                           last_time=ip_tracker.get_last_visited_time(ip_from))
+                           last_time=ip_tracker.get_last_visited_time(ip_from),
+                           resolution=resolution,
+                           browser_info=browser)
